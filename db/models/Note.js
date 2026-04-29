19836fe2ebcc507d947dc7920b538d046dde1c69
Kilian <kilian-wild@gmx.de>
2026-04-29 15:15:43 +0200

    feat: basic database connection setup

---
+++ db/models/Note.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const NoteSchema = new Schema({
+  _id: { type: String, required: true },
+  title: { type: String },
+  text: { type: String },
+  inquiry: { type: String },
+});
+
+const Note = mongoose.models.Note || mongoose.model("Note", NoteSchema);
+
+export default Note;
